8c65a5b2ba9a232e73a15cef68746ddbb2874398
TekiMeki <tekimekikot@gmail.com>
2022-01-19 17:12:00 +0200

    Changed name directory and add factory_method.py

---
+++ Creational_Patterns/factory_method.py
@@ -0,0 +1,33 @@
+class IceCreame:
+    def release(self, taste):
+        pass
+
+class Waffle(IceCreame):
+    def release(self, taste):
+        print(f"Вот вам вафельная мороженка со вкусом {taste}")
+
+class Stick(IceCreame):
+    def release(self, taste):
+        print(f"Вот вам мороженка на палочке со вкусом {taste}")
+
+class ICFactory:
+    def create(self)-> IceCreame:
+        pass
+
+class WaffleFactory(ICFactory):
+    def create(self):
+        return Waffle()
+
+class StickFactory(ICFactory):
+    def create(self):
+        return Stick()
+
+creator = WaffleFactory()
+waffle=creator.create()
+
+creator = StickFactory()
+stick=creator.create()
+
+waffle.release("клубничка")
+stick.release("шоколад")
+# print(waffle, stick, sep="\n")
